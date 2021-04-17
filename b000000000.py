f835d214b64782a4aa41789e7ce9125e199e25c3
dido05327 <idirabib@outlook.fr>
2021-04-17 01:15:47 -0400

    commit

--- b000000000.py
+++ b000000000.py
@@ -11,7 +11,47 @@ def charge(fichier):
       return json.load(f)
 
 def main():
- print(charge('b000000000.json'))
+   docs = lecture('b000000000.json')
+  print(len(docs.fetch_all()))
+
+   # Ne pas oublier de remercier le gestionnaire de BD
+  session.close
+
+
+import mysqlx
+
+session = mysqlx.get_session({
+    "host": "localhost",
+    "port": 33060,
+    "user": "root",
+    "password": "password"
+
+    db = session.get_schema("world_x")
+
+
+    def lecture(fichier):
+
+  # Le nom de la collection temporaire
+  nomColl = "temp"
+
+  # Crée la collection temporaire maColl
+  maColl = db.create_collection(nomColl)
+
+  # charge le fichier dans la variable json
+  json = charge(fichier)
+    
+  # Ajoute le contenu du fichier json dans la collection temporaire maColl
+  maColl.add(json).execute()
+
+  # Lis toute les documents convertis de la collection et les stocker dans la variable docs
+  docs = maColl.find().execute()
+
+  # Détruit la collection temporaire
+  db.drop_collection(nomColl)
+
+  # Retourne un dictionnaire Python du fichier json converti
+  return docs
+})
 
 if __name__== "__main__":
     main()
